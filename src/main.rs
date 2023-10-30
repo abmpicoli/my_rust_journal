43106d38e401d8a55cdbcb0ff2d615295c31b211
abmpicoli <abmpicoli@uol.com.br>
2023-10-30 07:14:45 -0300

    Journal.md review and side notes.
    Today I did re-read and reviewed the journal,
    and made some experiments about some notes I did
    before.
    
    There was this test about 'let being only usable on non-mutable
    variables' that was plain wrong.
    
    The oct_30_ramblings project shows an experiment where
    yes, we can call a let to shadow a mutable variable.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    // can I declare a mutable variable and then shadow it with a non-mutable one?
+    //
+    //
+    let mut x = 1;
+
+    x = x + 42;
+  
+    println!("Hello world {}",x);
+
+    // I'm shadowing the x with a non-mutable scope. Can I do it?
+    let x = 2;
+    // YES: I can. It is silly, though. And the compiler will warn about it.
+    
     println!("Hello, world!");
 }
